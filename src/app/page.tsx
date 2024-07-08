efd0a180e752ef81abc09fbe69eba9272a6014fd
Rajiv Ranjan Jha <rajiv.r@mahakaal.local>
2024-07-08 11:10:50 +0530

    fix api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,15 @@
-import PhotoDisplay from "./photo/[photoId]/PhotoDisplay"
-import type { PhotoData } from "./photo/[photoId]/page"
+import PhotoDisplay from "./photo/[photoId]/PhotoDisplay";
 
 export default async function Home() {
-
-  const response = await fetch('http://localhost:3500/images', { cache: 'no-store' })
-
-  const images: PhotoData[] = await response.json()
-
-  if (!images?.length) {
-    return <h1>No Images to Display</h1>
-  }
+  let data = await fetch("https://fakestoreapi.in/api/products")
+    .then((res) => res.json())
+    .then((res) => res);
 
   return (
-    <main className="flex flex-col items-center gap-8 pb-8">
-      {images.map(photoData => (
-        <PhotoDisplay key={photoData.id} photoData={photoData} />
+    <main className="flex flex-wrap gap-4 justify-center">
+      {data?.products?.map((product: any) => (
+        <PhotoDisplay data={product} key={product.id} />
       ))}
     </main>
-  )
-
+  );
 }
