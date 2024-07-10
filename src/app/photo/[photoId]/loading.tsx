abec12df01259e3d18a0bda6068671b1003700a1
Rajiv Ranjan Jha <rajiv.r@mahakaal.local>
2024-07-10 10:47:04 +0530

    client side loader

--- src/app/photo/[photoId]/loading.tsx
+++ src/app/photo/[photoId]/loading.tsx
@@ -1,5 +1,15 @@
-import { Loader } from "lucide-react";
+"use client";
+
+import {Loader} from "lucide-react";
+import {useEffect} from "react";
+
 export default function Loading() {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
   return (
     <div className="bg-black/40 fixed inset-0 flex items-center justify-center z-50">
       <Loader size="2rem" className="rotate" />
